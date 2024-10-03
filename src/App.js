503f2d67fcea284459d2ff49bc9674db14cde84d
Santhiya.K <162422504+zuppasanthiya@users.noreply.github.com>
2024-10-03 15:28:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [sections, setSections] = useState([]);
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+  const [showModal, setShowModal] = useState(false);
+
+  // Detect print screen or developer tools
+  useEffect(() => {
+    const handleKeydown = (e) => {
+      if (e.key === 'PrintScreen' || (e.ctrlKey && e.shiftKey && e.key === 'I') || e.key === 'F12') {
+        alert('Screenshot is disabled!');
+        setShowModal(true); // Show warning modal
+        e.preventDefault();
+      }
+    };
+
+    const handleCopyAttempt = () => {
+      setShowModal(true); // Show warning modal
+    };
+
+    const handleContextMenu = (e) => {
+      alert('Right-click is disabled!');
+      e.preventDefault(); // Prevent right-click
+    };
+
+    window.addEventListener('keydown', handleKeydown);
+    window.addEventListener('copy', handleCopyAttempt);
+    window.addEventListener('contextmenu', handleContextMenu);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+      window.removeEventListener('copy', handleCopyAttempt);
+      window.removeEventListener('contextmenu', handleContextMenu);
+    };
+  }, []);
+
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          {/* Video Element */}
+          <div className="video-container">
+          <video src="https://res.cloudinary.com/dmv2tjzo7/video/upload/v1725361412/rosh9hltomq8rbkyy3b2.mp4" controls width="900" height="500">
+            Your browser does not support the video tag.
+          </video>
+        </div>
     </div>
   );
 }
